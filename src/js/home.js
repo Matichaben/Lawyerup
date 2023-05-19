707f19d082f3d69e134c51d3c179515d0fa10cb6
Kingsley <kingsleymuturi9@gmail.com>
2023-05-19 15:43:29 +0300

    Add transition updates

--- src/js/home.js
+++ src/js/home.js
@@ -3,6 +3,8 @@ import { motion, useAnimation } from "framer-motion";
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import TrackChangesIcon from '@mui/icons-material/TrackChanges';
 import EngineeringIcon from '@mui/icons-material/Engineering';
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function Home(){
     const zoomIn = {
@@ -10,12 +12,30 @@ function Home(){
         hidden: { scale: 0},
     }
     const slideInTop = {
-        visible: { y:0, transition: {ease: "easeIn", duration: 1.5} },
-        hidden: { y: -300},
+        visible: { y: 0, opacity: 1, transition: {ease: "easeIn", duration: 1.5} },
+        hidden: { y: -250, opacity: 0},
     }
+    const fadeIn = {
+        visible: {opacity: 1, transition: {ease: "easeIn", duration: 1}},
+        hidden: {opacity: 0},
+    }
+    const control = useAnimation()
+    const [ref, inView] = useInView({threshold: 0})
+    useEffect(() => {
+        if (inView) {
+          control.start("visible");
+        } 
+        // else {
+        //     control.start("hidden");
+        // }      
+      }, [control, inView]);
     return(
         <div className="home">
-            <div className="video">
+            <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeIn}
+            className="video">
                 <div>
                     <video autoPlay muted loop id="myVideo">
                         <source src="lawyer.mp4" type="video/mp4"/>
@@ -24,7 +44,7 @@ function Home(){
                 <div className="title">
                     <h1 className="heading">Best law firm website</h1>
                 </div>
-            </div>
+            </motion.div>
             <div className="row">
                 <motion.div
                     variants={zoomIn}
@@ -41,8 +61,9 @@ function Home(){
                 </motion.div>
                 <motion.div
                     variants={slideInTop}
+                    ref={ref}
                     initial="hidden"
-                    animate="visible"
+                    animate={control}
                     className="col-md-4 mission">
                     <h2><TrackChangesIcon sx={{ marginBottom: 0.8}} fontSize="large"/> Our Mission</h2>
                     <p>We are committed to building a diverse workforce and 
@@ -54,8 +75,9 @@ function Home(){
                 </motion.div>
                 <motion.div
                     variants={zoomIn}
+                    ref={ref}
                     initial="hidden"
-                    animate="visible"
+                    animate={control}
                     className="col-md-4 services">
                     <h2><EngineeringIcon sx={{ marginBottom: 0.8}} fontSize="large"/> Services</h2>
                     <p>We have a wide range of services including:
